34c3e4f36c1cec343a39b1a8a9c05d058cbc5a70
mossadselim <mossad.selim11@gmail.com>
2024-11-26 14:07:26 +0200

    added font family

--- lib/src/root/app_root.dart
+++ lib/src/root/app_root.dart
@@ -2,6 +2,7 @@ import 'package:booky/core/helpers/theming/colors/color.dart';
 import 'package:booky/core/routing/app_router.dart';
 import 'package:booky/core/routing/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppRoot extends StatelessWidget {
   final AppRouter appRouter;
@@ -13,6 +14,7 @@ class AppRoot extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: AppColor.primaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
       ),
       initialRoute: Routes.splashScreen,
       onGenerateRoute: appRouter.generateRoute,
